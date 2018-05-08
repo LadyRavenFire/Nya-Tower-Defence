26a4f0f443dfca171ff8739174826c2410ed5760
LadyRavenFire <kilkust@gmail.com>
2018-05-09 00:34:54 +0500

    Check for enemies

---
+++ Tower-Defence/Assets/ArrowTower.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTower : MonoBehaviour
+{
+
+    private Transform _target;
+    public float Range = 10f;
+
+    public string EnemyTag = "Enemy";
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= Range)
+        {
+            _target = nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_target == null)
+	    {
+	        return;
+	    }
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
