0e9884c8926188d2c5edad64e904e22b34e58b4d
LadyRavenFire <kilkust@gmail.com>
2018-05-08 21:11:32 +0500

    Create Scene and Prefabs

---
+++ Tower-Defence/Assets/Scripts/WayPoints.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+
+    public static Transform[] Waypoints;
+
+    void Awake()
+    {
+        Waypoints = new Transform[transform.childCount];
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+
+}
